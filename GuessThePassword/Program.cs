06fd24094cb767ea864d0e190f4ee49a813c0896
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-12 11:12:27 +0300

    Initial commit

---
+++ GuessThePassword/Program.cs
@@ -0,0 +1,16 @@
+﻿
+class GuessPassword {
+    public static void Main (String[]args)
+    {
+
+        string password = Console.ReadLine();
+        
+        
+        if (password=="s3cr3t!") {
+            Console.WriteLine("Welcome");
+
+        }else {
+            Console.WriteLine("Wrong password!");
+        }
+    }
+}
